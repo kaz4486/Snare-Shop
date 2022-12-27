93ed3cf3c9703ab4be2f03b748e725cf24c25d61
Kamil <kaz4486@gmail.com>
2022-12-27 14:05:43 +0100

    Fix grid in searchProducts

--- client/src/components/views/SearchedProducts/SearchedProducts.js
+++ client/src/components/views/SearchedProducts/SearchedProducts.js
@@ -11,6 +11,7 @@ import { Progress } from 'reactstrap';
 import ProductSummary from '../../common/ProductSummary/ProductSummary';
 import styles from '../SearchedProducts/SearchedProducts.module.scss';
 import Spinner from 'react-bootstrap/Spinner';
+import arrayChunk from '../../../utils/arrayChunk';
 
 const SearchedProducts = () => {
   const { searchPhrase } = useParams();
@@ -46,18 +47,35 @@ const SearchedProducts = () => {
       <Container className={styles.container}>
         <h2 className={styles.heading}>Searched products</h2>
         <section>
-          {products.map((product) => (
-            <Col
-              key={product.id}
-              xs={12}
-              sm={6}
-              lg={4}
-              className="justify-content-center"
-            >
-              <ProductSummary key={product.id} {...product} />
-            </Col>
+          {arrayChunk(products, products.length).map((row, i) => (
+            <div key={i} className="row mx-auto">
+              {row.map((col, i) => (
+                <Col
+                  key={col.id}
+                  sm={12}
+                  md={6}
+                  xl={4}
+                  className="justify-content-center"
+                >
+                  <ProductSummary key={i} {...col} />
+                </Col>
+              ))}
+            </div>
           ))}
         </section>
+
+        {/* {arrayChunk(products, products.length).map((row, i) => (
+          <div key={i} className="row mx-auto">
+            {row.map((col, i) => (
+              <div
+                className="col-sm-12 col-md-6 col-xl-4 mb-2 d-flex justify-content-center"
+                key={col.id}
+              >
+                <ProductSummary key={i} {...col} />
+              </div>
+            ))}
+          </div>
+        ))} */}
       </Container>
     );
 };
