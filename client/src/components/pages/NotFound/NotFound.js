f6cee31eb9ab747762176b235b9d4bb2f66ce207
Kamil <kaz4486@gmail.com>
2022-12-27 13:22:07 +0100

    Modify NotFound

--- client/src/components/pages/NotFound/NotFound.js
+++ client/src/components/pages/NotFound/NotFound.js
@@ -1,5 +1,20 @@
+import { Container } from 'react-bootstrap';
+import styles from '../NotFound/NotFound.module.scss';
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <Container className="text-center">
+      <img
+        src={`${process.env.PUBLIC_URL}/images/404Image.jpg`}
+        alt="404 not found"
+        className={styles.img_not_found}
+      />
+      <br />
+      <a href="https://www.freepik.com/free-vector/404-error-with-cute-animal-concept-illustration_8030429.htm#query=404%20not%20found&position=34&from_view=keyword">
+        Image by storyset
+      </a>
+    </Container>
+  );
 };
 
 export default NotFound;
