903e888cacce8ccd7e993191b0ac6c0fb22fff0a
Kamil <kaz4486@gmail.com>
2023-06-08 13:15:57 +0200

    Rename files

--- client/src/components/features/CarouselGallery/CarouselGallery.js
+++ client/src/components/features/CarouselGallery/CarouselGallery.js
@@ -14,21 +14,21 @@ const CarouselGallery = () => {
   const slides = [
     <a href="/products/8d3dcd52-e923-4b57-8821-5a040fb3f51d">
       <img
-        src={`${process.env.PUBLIC_URL}/images/carousel/theodore1.png`}
+        src={`${process.env.PUBLIC_URL}/images/carousel/theodore1.jpg`}
         alt="john theodore"
         className={styles.img}
       />
     </a>,
     <a href="/products/337ae709-ddb4-4e3f-bfc0-0eb1ff52682a">
       <img
-        src={`${process.env.PUBLIC_URL}/images/carousel/chad2.png`}
+        src={`${process.env.PUBLIC_URL}/images/carousel/chad2.jpg`}
         alt="chad smiths"
         className={styles.img}
       />
     </a>,
     <a href="/products/32b311a7-7173-48a0-ade6-e9e8effcc91b">
       <img
-        src={`${process.env.PUBLIC_URL}/images/carousel/hawkins.png`}
+        src={`${process.env.PUBLIC_URL}/images/carousel/hawkins.jpg`}
         alt="taylor hawkins"
         className={styles.img}
       />
