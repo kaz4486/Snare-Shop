663fee718f8f6d7b9cd219995eac2d573cbe6bea
Kamil <kaz4486@gmail.com>
2023-06-08 12:09:22 +0200

    Rebuild

--- dist/main.js
+++ dist/main.js
@@ -3,8 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const common_1 = require("@nestjs/common");
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const fs = require("fs");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, {
+        httpsOptions: {
+            key: fs.readFileSync('path/to/private/key.pem'),
+            cert: fs.readFileSync('path/to/certificate.pem'),
+        },
+    });
     app.setGlobalPrefix('api');
     app.useGlobalPipes(new common_1.ValidationPipe({ transform: true }));
     await app.listen(process.env.PORT || 3000);
