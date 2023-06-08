dab2bdd6d9cd371aa6dd3b35e1c2508538dfa039
Kamil <kaz4486@gmail.com>
2023-06-08 12:54:29 +0200

    Change main.ts

--- dist/main.js
+++ dist/main.js
@@ -1,19 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const common_1 = require("@nestjs/common");
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
-const fs = require("fs");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule, {
-        httpsOptions: {
-            key: fs.readFileSync('path/to/private/key.pem'),
-            cert: fs.readFileSync('path/to/certificate.pem'),
-        },
-    });
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.setGlobalPrefix('api');
-    app.useGlobalPipes(new common_1.ValidationPipe({ transform: true }));
-    await app.listen(process.env.PORT || 3000);
+    await app.listen(3000);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,28 @@
-import { ValidationPipe } from '@nestjs/common';
+// import { ValidationPipe } from '@nestjs/common';
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import * as fs from 'fs';
+// import * as https from 'https';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule, {
+//     httpsOptions: {
+//       key: fs.readFileSync('path/to/private/key.pem'),
+//       cert: fs.readFileSync('path/to/certificate.pem'),
+//     },
+//   });
+//   app.setGlobalPrefix('api');
+//   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+//   await app.listen(process.env.PORT || 3000);
+// }
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as fs from 'fs';
-import * as https from 'https';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions: {
-      key: fs.readFileSync('path/to/private/key.pem'),
-      cert: fs.readFileSync('path/to/certificate.pem'),
-    },
-  });
+  const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(3000);
 }
 bootstrap();
