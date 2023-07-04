48b74a1fd0bef052bfc921096b43b33520a89e02
Kamil <kaz4486@gmail.com>
2023-07-04 18:04:50 +0200

    Change <a> to react-router-dom

--- client/src/components/features/CarouselGallery/CarouselGallery.js
+++ client/src/components/features/CarouselGallery/CarouselGallery.js
@@ -7,32 +7,36 @@ import '@brainhubeu/react-carousel/lib/style.css';
 import React, { useState } from 'react';
 import { Container } from 'react-bootstrap';
 import styles from './CarouselGallery.module.scss';
+import { Link } from 'react-router-dom';
 
 const CarouselGallery = () => {
   const [value, setValue] = useState(0);
 
   const slides = [
-    <a href="/products/8d3dcd52-e923-4b57-8821-5a040fb3f51d">
+    <Link to={'/products/8d3dcd52-e923-4b57-8821-5a040fb3f51d'}>
       <img
-        src={`${process.env.PUBLIC_URL}/images/carousel/theodore1.jpg`}
+        src={`${process.env.PUBLIC_URL}/images/carousel/Theodore1.jpg`}
         alt="john theodore"
         className={styles.img}
       />
-    </a>,
-    <a href="/products/337ae709-ddb4-4e3f-bfc0-0eb1ff52682a">
+    </Link>,
+
+    <Link to={'/products/337ae709-ddb4-4e3f-bfc0-0eb1ff52682a'}>
       <img
-        src={`${process.env.PUBLIC_URL}/images/carousel/chad2.jpg`}
+        src={`${process.env.PUBLIC_URL}/images/carousel/Chad2.jpg`}
         alt="chad smiths"
         className={styles.img}
       />
-    </a>,
-    <a href="/products/32b311a7-7173-48a0-ade6-e9e8effcc91b">
+    </Link>,
+
+    <Link to={'/products/32b311a7-7173-48a0-ade6-e9e8effcc91b'}>
+      {' '}
       <img
-        src={`${process.env.PUBLIC_URL}/images/carousel/hawkins.jpg`}
+        src={`${process.env.PUBLIC_URL}/images/carousel/Hawkins.jpg`}
         alt="taylor hawkins"
         className={styles.img}
       />
-    </a>,
+    </Link>,
   ];
 
   const onChange = (value) => {
