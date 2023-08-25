8b4c99cb6385b7cf035c0f13a44d21d7eb773b1b
Kamil <kaz4486@gmail.com>
2023-08-25 14:57:29 +0200

    Add icons in Header, Change layout in Header

--- client/src/components/layout/Header/Header.js
+++ client/src/components/layout/Header/Header.js
@@ -9,64 +9,75 @@ import {
   faPhoneAlt,
 } from '@fortawesome/free-solid-svg-icons';
 import { BiTimer } from 'react-icons/bi';
+import { HiDocumentText } from 'react-icons/hi';
+import { GiReturnArrow } from 'react-icons/gi';
 
 const Header = () => {
   return (
     <Navbar expand="lg" className={styles.navbar} variant="none">
-      <Container>
-        <Navbar.Brand as={NavLink} to="/" className={styles.navbar_brand}>
-          <img
-            src={`${process.env.PUBLIC_URL}/images/brandLogo.png`}
-            alt="brand logo"
-          />
-          SnareShop
-        </Navbar.Brand>
-        <Nav className={styles.nav}>
-          <Nav.Link as={NavLink} to="/" className={clsx(styles.link, 'mx-4')}>
-            <FontAwesomeIcon
-              icon={faHouse}
-              className={clsx(styles.icon, 'mx-2')}
-            />
-            Home
-          </Nav.Link>
-          <Nav.Link
-            as={NavLink}
-            to="/cart"
-            className={clsx(styles.link, 'mx-4')}
-          >
-            <FontAwesomeIcon
-              icon={faCartShopping}
-              className={clsx(styles.icon, 'mx-2')}
-            />
-            Cart
-          </Nav.Link>
-          <Row className={styles.icon_div}>
-            <Col className="p-0">
-              <FontAwesomeIcon
-                icon={faPhoneAlt}
-                className={clsx(styles.icon, 'mx-2')}
+      <Container className="m-1, d-flex, align-content-center">
+        <Row>
+          <Col sm={2}>
+            <Navbar.Brand as={NavLink} to="/" className={styles.navbar_brand}>
+              <img
+                src={`${process.env.PUBLIC_URL}/images/brandLogo.png`}
+                alt="brand logo"
               />
-            </Col>
-            <Col className="p-0">
-              <p className={styles.phone_number}> 258-515-978</p>
-            </Col>
-          </Row>
-        </Nav>
-        <div className={styles.shop_bonuses}>
-          <ul>
-            <li>
-              <BiTimer />
-              <span> Szybka realizacja</span>
-            </li>
-            <li>
-              <span>Wystawiamy faktury 23%</span>
-            </li>
-            <li>
-              {' '}
-              <span>Zwrot do 14 dni</span>
-            </li>
-          </ul>
-        </div>
+              SnareShop
+            </Navbar.Brand>
+          </Col>
+          <Col sm={7} className="justify-content-right">
+            <Nav className={styles.nav}>
+              <Nav.Link
+                as={NavLink}
+                to="/"
+                className={clsx(styles.link, 'mx-2')}
+              >
+                <FontAwesomeIcon
+                  icon={faHouse}
+                  className={clsx(styles.icon, 'mx-2')}
+                />
+                Home
+              </Nav.Link>
+              <Nav.Link
+                as={NavLink}
+                to="/cart"
+                className={clsx(styles.link, 'mx-2')}
+              >
+                <FontAwesomeIcon
+                  icon={faCartShopping}
+                  className={clsx(styles.icon, 'mx-2')}
+                />
+                Cart
+              </Nav.Link>
+              <div className={styles.icon_div}>
+                <FontAwesomeIcon
+                  icon={faPhoneAlt}
+                  className={clsx(styles.icon)}
+                />
+                <span className={styles.phone_number}> 258-515-978</span>
+              </div>
+            </Nav>
+          </Col>
+          <Col sm={3}>
+            <div className={styles.shop_bonuses}>
+              <ul>
+                <li>
+                  <BiTimer />
+                  <span>Szybka realizacja</span>
+                </li>
+                <li>
+                  <HiDocumentText />
+                  <span>Wystawiamy faktury 23%</span>
+                </li>
+                <li>
+                  <GiReturnArrow />
+                  <span>Zwrot do 14 dni</span>
+                </li>
+              </ul>
+            </div>
+          </Col>
+        </Row>
       </Container>
     </Navbar>
   );
